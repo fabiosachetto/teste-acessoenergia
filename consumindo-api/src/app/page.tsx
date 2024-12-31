886775c0f77e362c12a1749667c3afbbb920b018
fabiosachetto <fabio@sachetto.com.br>
2024-12-31 16:48:54 -0300

    arrumando pra mobile e adicionei o contator de tarefas

--- consumindo-api/src/app/page.tsx
+++ consumindo-api/src/app/page.tsx
@@ -112,17 +112,20 @@ export default function Home() {
 
   return (
     <div className="container">
-      <h2 className="mb-5 text-lg	font-bold uppercase">Todas as Tarefas</h2>
+      <div className="mb-5 flex justify-between">
+        <h2 className="text-lg font-bold uppercase">Tarefas</h2>
+        <p className="text-sm font-bold pt-1 px-2 border rounded-lg bg-blue-200 text-blue-600">{tarefasFiltradas.length}</p>
+      </div>
       <nav className="mb-5 rounded-lg bg-gray-100 border p-2">
-        <ol className="flex text-center">
-          <li className={`mr-2 p-2 border rounded-lg w-1/3 ${filtro === "todos" ? "bg-slate-500 text-white" : ""}`}>
-            <a href="#" className="block font-bold" onClick={() => setFiltro("todos")}>Todos</a>
+        <ol className="flex text-[12px] md:text-base text-black font-bold text-center justify-items-center">
+          <li className={`mr-2 p-2 border rounded-lg w-1/3 hover:text-white hover:bg-slate-500 ${filtro === "todos" ? "bg-slate-500 text-white" : ""}`}>
+            <a href="#" className="block" onClick={() => setFiltro("todos")}>Todos</a>
           </li>
-          <li className={`mr-2 p-2 border rounded-lg w-1/3 ${filtro === "em andamento" ? "bg-slate-500 text-white" : ""}`}>
-            <a href="#" className="block font-bold" onClick={() => setFiltro("em andamento")}>Em Andamento</a>
+          <li className={`mr-2 p-2 border rounded-lg w-1/3 hover:text-white hover:bg-slate-500 ${filtro === "em andamento" ? "bg-slate-500 text-white" : ""}`}>
+            <a href="#" className="block" onClick={() => setFiltro("em andamento")}>Em Andamento</a>
           </li>
-          <li className={`mr-2 p-2 border rounded-lg w-1/3 ${filtro === "concluida" ? "bg-slate-500 text-white" : ""}`}>
-            <a href="#" className="block font-bold" onClick={() => setFiltro("concluida")}>Concluído</a>
+          <li className={`mr-2 p-2 border rounded-lg w-1/3 hover:text-white hover:bg-slate-500 ${filtro === "concluida" ? "bg-slate-500 text-white" : ""}`}>
+            <a href="#" className="block" onClick={() => setFiltro("concluida")}>Concluído</a>
           </li>
         </ol>
       </nav>
